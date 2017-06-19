d339e0361654ba39db477747d4ebf6aff53b0281
vinicius <vinicius@hypermindr.com>
2017-06-19 17:16:55 -0300

    Added binary search and nlogn algorithm

--- intersection.py
+++ intersection.py
@@ -2,10 +2,29 @@ from random import randint
 from time import time
 
 
+def binary_search(x, ordered_list, start, end):
+    if start > end:
+        return False
+    central_pos = (start + end) // 2
+    central_element = ordered_list[central_pos]
+    if x == central_element:
+        return True
+    if x < central_element:
+        return binary_search(x, ordered_list, start, central_pos - 1)
+    return binary_search(x, ordered_list, central_pos + 1, end)
+
 def intersection_n2(list1, list2):
     count = 0
     for element in list1:
         if element in list2:
+            count += 1
+    return count
+
+def intersection_nlogn(list1, list2):
+    count = 0
+    list2.sort()
+    for element in list1:
+        if binary_search(element, list2, 0, len(list2) - 1):
             count += 1
     return count
 
@@ -30,17 +49,24 @@ def clock(method, arguments):
     start = time()
     result = method(*arguments)
     duration = time() - start
-    print("\nResult = %d (%s) --- elapsed: %.6f ms" %
-          (result, method.__name__, duration))
+    print("Result = %d --- elapsed: %.6f seconds (%s)" %
+          (result, duration, method.__name__))
 
 
 while True:
-    n = eval(input(" Size: " ))
+    n = eval(input("\nSize: " ))
+    if n == 0:
+        break
+    print("Generating lists..." )
     list1 = create_list(n, 100*n)
     list2 = create_list(n, 100*n)
+    print("Running intersection algorithms...")
     clock(intersection_n2, (list1, list2))
+    clock(intersection_nlogn, (list1, list2))
     clock(intersection_n, (list1, list2))
     print()
+
+print("\nCiao!")
     
 
 
